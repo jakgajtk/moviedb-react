9294af1561359e2061e596a76f252472e91c8325
Jakub Gajtkowski <jakub.gajtkowski@gmail.com>
2017-09-18 21:53:13 +0200

    Basic search implementation.

--- src/components/searchTab/searchTab.js
+++ src/components/searchTab/searchTab.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {config} from '../../configuration/config';
+import movieDb from 'themoviedb-javascript-library';
+
+// @todo - refactor to smaller components.
 
 export default class SearchTab extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            value: '',
+            items: []
+        };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.successCallback = this.successCallback.bind(this);
     }
-     handleChange(event) {
-        this.setState({value: event.target.value});
+
+    handleSearch() {
+        console.log('handling search');
+        movieDb.common.api_key = config.apiKey;
+        movieDb.search.getMovie(
+            {'query': this.state.textValue},
+            this.successCallback,
+            function() {}
+        )
+    }
+
+    successCallback(data) {
+        let arrayData = JSON.parse(data);
+        this.setState({ items: arrayData.results });
+    }
+
+    handleTextChange(event) {
+        this.setState({textValue: event.target.value})
     }
 
     render() {
+        let listItems = this.state.items.map(function(item) {
+            return (
+                <ListItem primaryText={item.original_title}/>
+            );
+        });
+
         return <div>
-            <input value="text" value={this.state.value} placeholder="search for movies..." onChange={this.handleChange} />
-            <List>
-                <ListItem primaryText="example list element"/>
-                <ListItem primaryText="example another list element"/>
-            </List>
-        </div>
+                    <TextField hintText="Search for movies" value={this.state.textValue} onChange={this.handleTextChange}/>
+                    <RaisedButton
+                        label="Search"
+                        onClick={this.handleSearch}
+                        primary={true}
+                    />
+                    <Divider />
+                    <List>
+                        {listItems}
+                    </List>
+                </div>
     }
  };
---
+++ src/configuration/config.js
@@ -0,0 +1,5 @@
+export const config = {
+    apiKey: '2e75df9a37110e4174bd86bef8018142',
+    posterPath: 'http://image.tmdb.org/t/p/w92',
+    moviePath: 'https://www.themoviedb.org/movie/'
+};
